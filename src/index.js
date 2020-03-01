b5adfeca50986f43317b08909bc241218af637a0
Robinson Lam <robinsonlam94@gmail.com>
2020-03-01 15:19:20 +1100

    add form and license

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import fullpage from 'fullpage.js';
+import { $ } from 'jquery';
 
-// STyles
+// Styles
 import resetStyles from './styles/reset.css';
 import styles from './styles/main.css';
 
@@ -11,3 +12,21 @@ var fullPageInstance = new fullpage('#fullpage', {
     navigation: true,
     sectionsColor:['#ffffff', '#d1e3ff', '#ffffff', 'd1e3ff', '#ffffff', 'd1e3ff']
 });
+
+
+// RSVP FORM SCRIPT
+var $form = $('form#weddingRsvpForm'),
+    url = 'https://script.google.com/macros/s/AKfycbzKBO9PBckRlXfRPiruLpq2yOj6MwwlSUcDOHGlkoNfMtWntrqJ/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+
+})
